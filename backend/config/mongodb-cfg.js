3c32d625dcf33598c212a0c13e63f5f91f57fe21
sherikovic <sherif.amer01@gmail.com>
2024-01-25 13:13:09 +0200

    mongodb went online

--- backend/config/mongodb-cfg.js
+++ backend/config/mongodb-cfg.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/elementsDB');
+const username = encodeURIComponent("sherikovic");
+const password = encodeURIComponent("A1234a4321");
+const mongoDBUrl = `mongodb+srv://${username}:${password}@cluster0.ah7ultu.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoDBUrl);
 const db = mongoose.connection;
 db.on('error', (err) => { console.error("Connection error:", err) });
 db.once('open', () => { console.log("Database connected"); });
--- backend/seeds/elementsDummy.js
+++ backend/seeds/elementsDummy.js
@@ -6,12 +6,12 @@ module.exports = [
     },
     {
         name: "Maps_BLK_API_Key",
-        description: "use this to block enable maps on your website",
+        description: "use this to block maps on your website",
         value: "XIUnfwfakfgAUSBsdajbfa"
     },
     {
         name: "Maps_DEL_API_Key",
-        description: "use this to delete enable maps on your website",
+        description: "use this to delete maps on your website",
         value: "sadiOANfakfgAUSBsdajbfa"
     }
 ];
