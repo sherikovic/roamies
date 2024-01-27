11bdb446b92fe448211809b568cf0503de5e2c68
Natalia <ngabuaeva@gmail.com>
2024-01-26 22:52:38 -0500

    Change styles

--- frontend/src/Components/AccountSettingsForm.tsx
+++ frontend/src/Components/AccountSettingsForm.tsx
@@ -1,18 +1,18 @@
-import { Form, NavLink } from 'react-router-dom';
-import { useState } from 'react';
-import APIFormTextInputLabel from './APIFormTextInputLabel';
-import classes from './APInfoForm.module.css';
+import { Form } from "react-router-dom";
+import { useState } from "react";
+import APIFormTextInputLabel from "./APIFormTextInputLabel";
+import classes from "./APInfoForm.module.css";
 
 const AccountSettingsForm: React.FC = () => {
-    const [changeEmail, setChangeEmail] = useState(true);
-    const [changePassword, setChangePassword] = useState(false);
+  const [changeEmail, setChangeEmail] = useState(true);
+  const [changePassword, setChangePassword] = useState(false);
 
-    return (
-        <div className={classes.api_form}>
-            <p className={classes.api_form_header}>What would you like to change?</p>
-            <div className={classes.api_form_container}>
-                <div className={classes.api_form_btns_sel}>
-                    {/* <NavLink
+  return (
+    <div className={classes.api_form}>
+      <p className={classes.api_form_header}>What would you like to change?</p>
+      <div className={classes.api_form_container}>
+        <div className={classes.api_form_btns_sel}>
+          {/* <NavLink
                         to="#"
                         className={({ isActive }) =>
                         isActive ?
@@ -20,92 +20,94 @@ const AccountSettingsForm: React.FC = () => {
                         >
                         Update my email address
                     </NavLink> */}
-                    <button onClick={
-                        () => {
-                            changePassword && setChangeEmail(true);
-                            setChangePassword(false)
-                        }
-                    }
-                    className={classes.api_form_btn_sel}
-                    >
-                        Update Email Address
-                    </button>
-                    <button onClick={
-                        () => {
-                            changeEmail && setChangePassword(true);
-                            setChangeEmail(false)
-                        }
-                    }
-                    className={classes.api_form_btn_sel}
-                    >
-                        Update Password
-                    </button>
-                </div>
-                {changeEmail &&
-                    <Form method='patch' className={classes.api_form_fr}>
-                        <APIFormTextInputLabel
-                            type='email'
-                            name='old Email'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <APIFormTextInputLabel
-                            type='email'
-                            name='new Email'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <APIFormTextInputLabel
-                            type='email'
-                            name='confirm New Email'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <div className={classes.api_btn_save}>
-                            <button>Save</button>
-                        </div>
-                    </Form>
-                }
-                {changePassword &&
-                    <Form method='patch' className={classes.api_form_fr}>
-                        <APIFormTextInputLabel
-                            type='password'
-                            name='old Password'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <APIFormTextInputLabel
-                            type='password'
-                            name='new Password'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <APIFormTextInputLabel
-                            type='password'
-                            name='confirm New Password'
-                            value=''
-                            classDiv={classes.api_form_item}
-                            classInput={classes.api_input_field}
-                            classLabel={classes.api_input_label}
-                        />
-                        <div className={classes.api_btn_save}>
-                            <button>Save</button>
-                        </div>
-                    </Form>
-                }
-            </div>
+          <button
+            onClick={() => {
+              changePassword && setChangeEmail(true);
+              setChangePassword(false);
+            }}
+            className={`${classes.api_form_btn_sel} ${
+              changeEmail && classes.selected
+            }`}
+          >
+            Update Email Address
+          </button>
+          <button
+            onClick={() => {
+              changeEmail && setChangePassword(true);
+              setChangeEmail(false);
+            }}
+            className={`${classes.api_form_btn_sel} ${
+              changePassword && classes.selected
+            }`}
+          >
+            Update Password
+          </button>
         </div>
-    );
+        {changeEmail && (
+          <Form method="patch" className={classes.api_form_fr}>
+            <APIFormTextInputLabel
+              type="email"
+              name="old Email"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <APIFormTextInputLabel
+              type="email"
+              name="new Email"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <APIFormTextInputLabel
+              type="email"
+              name="confirm New Email"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <div className={classes.api_btn_save}>
+              <button>Save</button>
+            </div>
+          </Form>
+        )}
+        {changePassword && (
+          <Form method="patch" className={classes.api_form_fr}>
+            <APIFormTextInputLabel
+              type="password"
+              name="old Password"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <APIFormTextInputLabel
+              type="password"
+              name="new Password"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <APIFormTextInputLabel
+              type="password"
+              name="confirm New Password"
+              value=""
+              classDiv={classes.api_form_item}
+              classInput={classes.api_input_field}
+              classLabel={classes.api_input_label}
+            />
+            <div className={classes.api_btn_save}>
+              <button>Save</button>
+            </div>
+          </Form>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AccountSettingsForm;
--- frontend/src/Components/MainNavigation.tsx
+++ frontend/src/Components/MainNavigation.tsx
@@ -1,109 +1,129 @@
 import { Form, NavLink, Link, useRouteLoaderData } from "react-router-dom";
 import { useRef, useState } from "react";
 
-import classes from './MainNavigation.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretSquareDown } from '@fortawesome/free-regular-svg-icons';
+import classes from "./MainNavigation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretSquareDown } from "@fortawesome/free-regular-svg-icons";
 import { useOutsideAlerter } from "../utils";
 
 const MainNavigation: React.FC = () => {
-    const data: any = useRouteLoaderData('root');
-    const [toggleDisplay, setToggleDisplay] = useState(false);
-    const containerRef = useRef(null);
+  const data: any = useRouteLoaderData("root");
+  const [toggleDisplay, setToggleDisplay] = useState(false);
+  const containerRef = useRef(null);
 
-    useOutsideAlerter(
-        containerRef,
-        () => {
-            setToggleDisplay(false);
-        }
-    );
+  useOutsideAlerter(containerRef, () => {
+    setToggleDisplay(false);
+  });
 
-    function dropdownContentToggleHandler(event: any): void {
-        setToggleDisplay(!toggleDisplay);
-    }
+  function dropdownContentToggleHandler(event: any): void {
+    setToggleDisplay(!toggleDisplay);
+  }
 
-    return (
-        <header className={classes.header}>
-            <nav className={classes.list}>
-                <NavLink
-                    to='/'
-                    className={({ isActive }) =>
-                        isActive ?
-                            `${classes.listitem} ${classes.listitem_active}` : classes.listitem}
-                    end
-                >
-                    Home
-                </NavLink>
-                <NavLink
-                    to='/elements'
-                    className={({ isActive }) =>
-                        isActive ?
-                            `${classes.listitem} ${classes.listitem_active}` : classes.listitem}
-                >
-                    Elements
-                </NavLink>
-            </nav>
-            <nav ref={containerRef} className={classes.dropdown}>
-                <div
-                    className={classes.dd_user_menu}
-                    onClick={dropdownContentToggleHandler}
-                >
-                    <img
-                        src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"
-                        alt="user's profile"
-                        onClick={dropdownContentToggleHandler}
-                    />
-                    {data && data.user &&
-                        <i className={classes.dd_userheader}>Hello, {data.user}</i>
-                    }
-                    <div className={`${classes.dd_caret_down} ${toggleDisplay && classes.dd_caret_down_tr}`}>
-                        <FontAwesomeIcon icon={faCaretSquareDown} />
-                    </div>
-                </div>
-                {(!data || !data.user) &&
-                    <div className={`${classes.dd_content} ${toggleDisplay && classes.dd_content_display}`}>
-                        <Link
-                            to='/auth?mode=login'
-                            onClick={dropdownContentToggleHandler}
-                            className={classes.dd_content_item}
-                        >
-                            Login
-                        </Link>
-                    </div>
-                }
-                {data && data.user &&
-                    <div className={`${classes.dd_content} ${toggleDisplay && classes.dd_content_display}`}>
-                        <NavLink
-                            to='/profile'
-                            onClick={dropdownContentToggleHandler}
-                            className={({ isActive }) =>
-                                isActive ?
-                                    `${classes.dd_content_item} ${classes.usermenu_active}` : classes.dd_content_item}
-                        >
-                            Profile
-                        </NavLink>
-                        <NavLink
-                            to='/settings'
-                            onClick={dropdownContentToggleHandler}
-                            className={({ isActive }) =>
-                                isActive ?
-                                    `${classes.dd_content_item} ${classes.usermenu_active}` : classes.dd_content_item}
-                        >
-                            Settings
-                        </NavLink>
-                        <Form action="/logout" method="post">
-                            <button
-                                onClick={dropdownContentToggleHandler}
-                                className={`${classes.dd_content_item} ${classes.dd_content_button}`}
-                            >
-                                Logout
-                            </button>
-                        </Form>
-                    </div>
-                }
-            </nav>
-        </header >
-    );
+  return (
+    <header className={classes.header}>
+      <nav className={classes.list}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? `${classes.listitem} ${classes.listitem_active}`
+              : classes.listitem
+          }
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/elements"
+          className={({ isActive }) =>
+            isActive
+              ? `${classes.listitem} ${classes.listitem_active}`
+              : classes.listitem
+          }
+        >
+          Elements
+        </NavLink>
+      </nav>
+      <nav ref={containerRef} className={classes.dropdown}>
+        <div
+          className={classes.dd_user_menu}
+          onClick={dropdownContentToggleHandler}
+        >
+          <img
+            src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"
+            alt="user's profile"
+            onClick={dropdownContentToggleHandler}
+          />
+          {data && data.user && (
+            <i className={classes.dd_userheader}>Hello, {data.user}</i>
+          )}
+          <div
+            className={`${classes.dd_caret_down} ${
+              toggleDisplay && classes.dd_caret_down_tr
+            }`}
+          >
+            <FontAwesomeIcon
+              icon={faCaretSquareDown}
+              className={classes.dd_caret_down_svg}
+            />
+          </div>
+        </div>
+        {(!data || !data.user) && (
+          <div
+            className={`${classes.dd_content} ${
+              toggleDisplay && classes.dd_content_display
+            }`}
+          >
+            <Link
+              to="/auth?mode=login"
+              onClick={dropdownContentToggleHandler}
+              className={classes.dd_content_item}
+            >
+              Login
+            </Link>
+          </div>
+        )}
+        {data && data.user && (
+          <div
+            className={`${classes.dd_content} ${
+              toggleDisplay && classes.dd_content_display
+            }`}
+          >
+            <NavLink
+              to="/profile"
+              onClick={dropdownContentToggleHandler}
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.dd_content_item} ${classes.usermenu_active}`
+                  : classes.dd_content_item
+              }
+            >
+              Profile
+            </NavLink>
+            <NavLink
+              to="/settings"
+              onClick={dropdownContentToggleHandler}
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.dd_content_item} ${classes.usermenu_active}`
+                  : classes.dd_content_item
+              }
+            >
+              Settings
+            </NavLink>
+            <Form action="/logout" method="post">
+              <button
+                onClick={dropdownContentToggleHandler}
+                className={`${classes.dd_content_item} ${classes.dd_content_button}`}
+              >
+                Logout
+              </button>
+            </Form>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 };
 
 export default MainNavigation;
