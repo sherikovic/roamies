378bc8cabfdef07ff390a5c4811070ad3ad0a8c9
sherikovic <sherif.amer01@gmail.com>
2024-03-16 00:05:35 +0200

    .

--- frontend/src/Pages/About.tsx
+++ frontend/src/Pages/About.tsx
@@ -1,5 +1,4 @@
 import UnderConstructionPage from 'Components/UnderConstruction';
-import classes from './About.module.css';
 
 const AboutPage: React.FC = () => {
 	return <UnderConstructionPage />;
