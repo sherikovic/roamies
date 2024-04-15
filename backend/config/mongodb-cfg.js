e88630bbf58f897015c1dc399cab3468d8991e02
sherikovic <sherif.amer01@gmail.com>
2024-04-16 00:39:26 +0300

    db online

--- backend/config/mongodb-cfg.js
+++ backend/config/mongodb-cfg.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const localUrl = "mongodb://127.0.0.1:27017/playground";
 const onlineUrl =
 	"mongodb+srv://sherikovic:A1234a4321@cluster0.ah7ultu.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(localUrl);
+mongoose.connect(onlineUrl);
 const db = mongoose.connection;
 db.on("error", (err) => {
 	console.error("Connection error:", err);
