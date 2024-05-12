12e275afa7f991e9d8bfa99d5d7b66b56404d3d6
Natalia <ngabuaeva@gmail.com>
2024-05-12 19:44:59 +0300

    Wip

--- frontend/src/Components/Settings/PersonalInfoForm.tsx
+++ frontend/src/Components/Settings/PersonalInfoForm.tsx
@@ -4,81 +4,100 @@ import classes from "./APInfoForm.module.css";
 import { User } from "types/user";
 
 interface PersonalInfoFormProps {
-	userData: User;
-	children?: React.ReactNode;
+  userData: User;
+  children?: React.ReactNode;
 }
 
 const PersonalInfoForm: React.FC<PersonalInfoFormProps> = (props) => {
-	const data: any = useActionData();
-	const userInfo = props.userData;
-	console.log(userInfo);
+  const data: any = useActionData();
+  const userInfo = props.userData;
+  console.log(userInfo);
 
-	return (
-		<Form method="patch">
-			<div className={classes.api_form}>
-				<p className={classes.api_form_header}>
-					Update your personal information
-				</p>
-				{data && data.errorMessage && data.type === "personal" && (
-					<p className={classes.api_form_header} style={{ color: "orange" }}>
-						{data.errorMessage}
-					</p>
-				)}
-				{data && data.successMessage && data.type === "personal" && (
-					<p className={classes.api_form_header} style={{ color: "green" }}>
-						{data.successMessage}
-					</p>
-				)}
-				<APIFormTextInputLabel
-					type="textarea"
-					name="bio"
-					value={props.userData ? userInfo.bio : ""}
-					classDiv={classes.api_form_item}
-					classInput={classes.api_input_field}
-					classLabel={classes.api_input_label}
-				/>
-				<div className={classes.api_form_group_cc}>
-					<APIFormTextInputLabel
-						type="text"
-						name="first name"
-						value={props.userData ? userInfo.firstname : ""}
-						classDiv={classes.api_form_item}
-						classInput={classes.api_input_field}
-						classLabel={classes.api_input_label}
-					/>
-					<APIFormTextInputLabel
-						type="text"
-						name="last name"
-						value={props.userData ? userInfo.lastname : ""}
-						classDiv={classes.api_form_item}
-						classInput={classes.api_input_field}
-						classLabel={classes.api_input_label}
-					/>
-				</div>
-				<div className={classes.api_form_group_cc}>
-					<APIFormTextInputLabel
-						type="text"
-						name="age"
-						value={props.userData ? userInfo.age : ""}
-						classDiv={classes.api_form_item}
-						classInput={classes.api_input_field}
-						classLabel={classes.api_input_label}
-					/>
-					<APIFormTextInputLabel
-						type="text"
-						name="country"
-						value={props.userData ? userInfo.country : ""}
-						classDiv={classes.api_form_item}
-						classInput={classes.api_input_field}
-						classLabel={classes.api_input_label}
-					/>
-				</div>
-				<div className={classes.api_btn_save}>
-					<button name={"personal"}>Save</button>
-				</div>
-			</div>
-		</Form>
-	);
+  return (
+    <Form method="patch">
+      <div className={classes.api_form}>
+        <p className={classes.api_form_header}>
+          Update your personal information
+        </p>
+        {data && data.errorMessage && data.type === "personal" && (
+          <p className={classes.api_form_header} style={{ color: "orange" }}>
+            {data.errorMessage}
+          </p>
+        )}
+        {data && data.successMessage && data.type === "personal" && (
+          <p className={classes.api_form_header} style={{ color: "green" }}>
+            {data.successMessage}
+          </p>
+        )}
+        <div className={classes.api_form_group_cc}>
+          <APIFormTextInputLabel
+            type="text"
+            name="first name"
+            value={props.userData ? userInfo.firstname : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+          <APIFormTextInputLabel
+            type="text"
+            name="last name"
+            value={props.userData ? userInfo.lastname : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+        </div>
+        <div className={classes.api_form_group_cc}>
+          <APIFormTextInputLabel
+            type="text"
+            name="age"
+            value={props.userData ? userInfo.age : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+          <APIFormTextInputLabel
+            type="text"
+            name="country"
+            value={props.userData ? userInfo.country : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+        </div>
+        <APIFormTextInputLabel
+          type="textarea"
+          name="bio"
+          value={props.userData ? userInfo.bio : ""}
+          classDiv={classes.api_form_item}
+          classInput={classes.api_input_field}
+          classLabel={classes.api_input_label}
+        />
+        <p className={classes.api_form_header}>Socials</p>
+        <div className={classes.api_form_group_cc}>
+          <APIFormTextInputLabel
+            type="text"
+            name="instagram"
+            value={props.userData ? userInfo.social?.instagram : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+          <APIFormTextInputLabel
+            type="text"
+            name="twitter"
+            value={props.userData ? userInfo.social?.twitter : ""}
+            classDiv={classes.api_form_item}
+            classInput={classes.api_input_field}
+            classLabel={classes.api_input_label}
+          />
+        </div>
+        <div className={classes.api_btn_save}>
+          <button name={"personal"}>Save</button>
+        </div>
+      </div>
+    </Form>
+  );
 };
 
 export default PersonalInfoForm;
