d63ebefcb720b403dc8499034e4f3eeaa4cb0073
sherikovic <sherif.amer01@gmail.com>
2025-02-13 16:32:35 -0500

    Enhance LandingNavigation component; add responsive logo text size adjustments

--- frontend/src/Components/Landingpage/LandingNavigation.tsx
+++ frontend/src/Components/Landingpage/LandingNavigation.tsx
@@ -8,12 +8,15 @@ const LandingNavigation: React.FC = () => {
     if (isMobile) return
     const nav = document.getElementById('navbar-container')
     const logoElem = document.getElementById('logo-elem')
+    const logoText = document.getElementById('logo-text')
     const navLinks = document.getElementById('nav-links')
     if (this.scrollY >= 200) {
       nav?.classList.remove('nav-normal')
       nav?.classList.add('nav-scroll')
       logoElem?.classList.remove('logo-large')
       logoElem?.classList.add('logo-small')
+      logoText?.classList.remove('logo-text-large')
+      logoText?.classList.add('logo-text-small')
       navLinks?.classList.remove('nav-links-normal')
       navLinks?.classList.add('nav-links-scroll')
     } else {
@@ -21,6 +24,8 @@ const LandingNavigation: React.FC = () => {
       nav?.classList.add('nav-normal')
       logoElem?.classList.remove('logo-small')
       logoElem?.classList.add('logo-large')
+      logoText?.classList.remove('logo-text-small')
+      logoText?.classList.add('logo-text-large')
       navLinks?.classList.remove('nav-links-scroll')
       navLinks?.classList.add('nav-links-normal')
     }
@@ -30,7 +35,7 @@ const LandingNavigation: React.FC = () => {
     <div id="navbar-container" className="nav-normal">
       <div className="logo-container">
         <SVG src={logo} id="logo-elem" className="logo-large" />
-        <p id="logo-elem" className="logo-large">
+        <p id="logo-text" className="logo-text-large">
           Roamies
         </p>
       </div>
